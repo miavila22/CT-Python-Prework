962c75d472f9a7d852fe689a5d2c6014991e8f5b
Miguel <miguelavila93@yahoo.com>
2023-09-24 14:55:56 -0500

    first commit

---
+++ Hello.py
@@ -0,0 +1,41 @@
+# Create a function that takes a name as an arguement and
+# returns a new string that says Hello and then the name
+# that was the argument
+# Then use this function and ask the user for their name
+# and tell them hello
+
+print("John")
+
+name = "John"
+print(name)
+
+print("Hello", name)
+print("Hello" + " " + name)
+hello_John = "Hello" + " " + name
+print(hello_John)
+
+print("are function is defined below")
+def say_hello_print(the_users_name):
+    print("Hello " + the_users_name)
+    return None
+
+print("Using our function with the print statement inside")
+say_hello_print("John")
+print(say_hello_print ("John"))
+
+def say_hello_return(the_users_name) :
+    return "Hello " + the_users_name
+
+print("Using our function with the return statement inside and not printing")
+print(say_hello_return("Steve") == "Hello Steve")
+
+
+print(say_hello_return("Steve"))
+hello_steve = say_hello_return("Steve")
+print(hello_steve) 
+
+
+users_name = input("What is your name? ")
+# print(users_name)
+# print(type(users_name))
+print(say_hello_return(users_name))
---
+++ number_adder.py
@@ -0,0 +1,13 @@
+# Create a fucntion what will add up all the numbers in a 
+# list of numbers that was give as an argument
+# Use this function to ask the user for aseries of number
+# and then show the user the sum of all the numbers
+# that were given by the user
+
+#          0,1,2, 3, 4
+my_nums = [1,8,9,10,15]
+
+print(my_nums)
+print(my_nums[0])
+print(my_nums[4])
+print(type(my_nums))
